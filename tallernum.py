0e3e6f2592cb21351d06cd132dd578c8fdfc3747
ssalamanca <santiago.anime@hotmail.com>
2017-10-20 10:03:08 -0500

    Taller Punto 3

--- tallernum.py
+++ tallernum.py
@@ -4,7 +4,7 @@ import math
 
 def integrar(a,b):
     x2= lambda x: math.sqrt(x)*math.sin(x)
-    print (integrate.quad(x2,0,4))
+   # print (integrate.quad(x2,0,4))
     return (integrate.quad(x2,0,4))
     
 def f(x):
@@ -15,23 +15,22 @@ def trapecios(a,b,m):
     s = 0
     for i in range(1,m):
         s = s + f(a + i * h)
-        print(s)
+        #print(s)
     r = h / 2 *(f(a)+2*s+f(b))
     return r
 
 a=0
 b=2
 m=[10,100,1000]
+print('%18.15s %18.15s %18.15s %18.15s %18.15s'%("m","trap","real","e truncamiento","e absoluto"))
 for i in range(0,3):
-    print(i)
+    #print(i)
     r=trapecios(a,b,m[i])
     z=integrar(a,b)
     #print(z[0])
     #print(z[1])
-    res=z[1]-z[0]
-    print(res)
-    """t = ((-((b-a)/m[i])**2)/12)(b-a)**3"""
+    res=z[0]-z[1]
+   # print(res)
+    t = ((-((b-a)/m[i])**2)/12)*(b-a)**3
     e=res-r
-    print('%18.15f %18.15f %18.15f'%(r,res,e))
-
-
+    print('%18.15s %18.15f %18.15f %18.15f %18.15f'%(m[i],r,res,e,t))
