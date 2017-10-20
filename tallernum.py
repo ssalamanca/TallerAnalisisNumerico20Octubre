eed846ec80272323200d436a0c10167768feecdb
ssalamanca <santiago.anime@hotmail.com>
2017-10-20 09:28:19 -0500

    Add files via upload

--- tallernum.py
+++ tallernum.py
@@ -5,6 +5,7 @@ import math
 def integrar(a,b):
     x2= lambda x: math.sqrt(x)*math.sin(x)
     print (integrate.quad(x2,0,4))
+    return (integrate.quad(x2,0,4))
     
 def f(x):
     return math.sqrt(x)*math.sin(x)
@@ -14,17 +15,23 @@ def trapecios(a,b,m):
     s = 0
     for i in range(1,m):
         s = s + f(a + i * h)
-        r = h / 2 *(f(a)+2*s+f(b))
+        print(s)
+    r = h / 2 *(f(a)+2*s+f(b))
     return r
 
 a=0
 b=2
 m=[10,100,1000]
-for i in range(0,2):
+for i in range(0,3):
+    print(i)
     r=trapecios(a,b,m[i])
     z=integrar(a,b)
+    #print(z[0])
+    #print(z[1])
+    res=z[1]-z[0]
+    print(res)
     """t = ((-((b-a)/m[i])**2)/12)(b-a)**3"""
-    e=z-r
-    print('%18.15s %18.15s %18.15s'%(r,z,e))
+    e=res-r
+    print('%18.15f %18.15f %18.15f'%(r,res,e))
 
 
